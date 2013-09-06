688009e6f0be6432d26eac73111b7711a06753ad
Michael van Holst <mikevanholst@gmail.com>
2013-09-06 17:00:59 -0400

    adds working validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,30 @@
 class ProductsController < ApplicationController
+ 
   def index
  	@products = Product.all
+
+ 	respond_to do |format|
+ 		format.html
+ 		format.json {render json: @products}
+ 	end	
   end
 
   def show
   	@product = Product.find(params[:id])
+
+  	respond_to do |format|
+ 		format.html
+ 		format.json {render json: @product}
+ 	end	
   end
 
   def new
   	@product = Product.new
+ 
+  	respond_to do |format|
+ 		format.html
+ 		format.json {render json: @product}
+ 	end	
   end
 
   def edit
@@ -19,11 +35,16 @@ class ProductsController < ApplicationController
   def create
 	  @product = Product.new(params[:product])
 
-	  if @product.save
-	  	redirect_to products_url
-	  else
-	  	render :new
-	  end	
+	  respond_to do |format|
+		  if @product.save
+		  	format.html { redirect_to @product, notice: "Product was successfully created." }
+		  	format.json { render json: @product, status: :created, location: @product }
+		  else
+		  	format.html { render action: "new" }
+		  	format.json { render json: @product.errors, status: :unprocessable_entity }
+
+		  end
+	end	  	
   end
 
 	def update
